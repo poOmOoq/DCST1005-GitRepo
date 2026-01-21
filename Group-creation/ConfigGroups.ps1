9cd8f26c7bb727fca39d89ba6547f7d6f32a443f
Kevin Tran <kevintran03.06.04@gmail.com>
2026-01-21 12:33:03 +0100

    Completed member adding

--- Group-creation/ConfigGroups.ps1
+++ Group-creation/ConfigGroups.ps1
@@ -3,36 +3,6 @@
 
 Write-Host "Running Config..." -ForegroundColor Blue
 
-function findPathToTarget {
-    param (
-        [Parameter(Mandatory)]
-        [hashtable]
-        $struct,
-        [Parameter(Mandatory)]
-        [string]
-        $current_path,
-        [Parameter(Mandatory)]
-        [string]
-        $targ
-    )
-        
-    $current_OU = $struct.Keys[0]
-        
-    $children_OU = $struct[$current_OU][0]
-        
-    if ($children_OU.Contains($targ)) {
-        return "OU=$targ,OU=$current_OU,$current_path"
-    }
-    
-    $current_path = "OU=$current_OU,$current_path"
-    
-    $children_OU | ForEach-Object {
-        if ($_.GetType().Name -ceq $hashtable_flag) {
-            return findPathToTarget -struct $_ -current_path $current_path -targ $targ
-        }
-    }
-}
-
 $target = "InfraIT_Groups"
 
 $group_path = findPathToTarget -struct $structure -current_path $domain_path -targ $target
@@ -40,31 +10,31 @@ $group_path = findPathToTarget -struct $structure -current_path $domain_path -ta
 # Define your groups with their properties
 $groups = @(
     @{
-        Name     = "HR Team"
+        Name     = "HR"
         Path     = $group_path
         Scope    = "Global"
         Category = "Security"
     },
     @{
-        Name     = "IT Team"
+        Name     = "IT"
         Path     = $group_path
         Scope    = "Global"
         Category = "Security"
     },
     @{
-        Name     = "Sales Team"
+        Name     = "Sales"
         Path     = $group_path
         Scope    = "Global"
         Category = "Security"
     },
     @{
-        Name     = "Finance Team"
+        Name     = "Finance"
         Path     = $group_path
         Scope    = "Global"
         Category = "Security"
     },
     @{
-        Name     = "Consultants Team"
+        Name     = "Consultants"
         Path     = $group_path
         Scope    = "Global"
         Category = "Security"
